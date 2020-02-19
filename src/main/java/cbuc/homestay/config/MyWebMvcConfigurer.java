71cafb068af5a0fc947e4c988e32c69eb1eaf9cb
Caiwx <1404668149@qq.com>
2020-02-19 15:30:34 +0800

    添加拦截器  commit

--- src/main/java/cbuc/homestay/config/MyWebMvcConfigurer.java
+++ src/main/java/cbuc/homestay/config/MyWebMvcConfigurer.java
@@ -6,10 +6,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.nio.charset.Charset;
+import java.util.Arrays;
 
 /**
  * @Explain: web配置类
@@ -30,25 +32,25 @@ public class MyWebMvcConfigurer implements WebMvcConfigurer {
     }
 
     /**
-     * @Explain 添加虚拟路径的映射
      * @param registry
+     * @Explain 添加虚拟路径的映射
      */
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/static/**"). addResourceLocations("classpath:/static/");
-        String uploadPath = "file:D://Cbuc_homestay/upload/";
-        registry.addResourceHandler("/upfiles/**").addResourceLocations(uploadPath);
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
     }
 
     /**
-     * @Explain 添加拦截器链      //TODO   开发接口未测试接口暂时关闭该拦截器，上线后请开启
      * @param registry
+     * @Explain 添加拦截器链
      */
-    /*@Override
+    @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loginInterceptor)
-                .addPathPatterns("/**")
+                .addPathPatterns("/merchant/**")
+                .addPathPatterns("/admin/**")
+                .addPathPatterns(Arrays.asList("/home/**", "toModPwd", "uploadImg", "sendMsg", "toMsgHistory", "toMessageCenter", "msgCenterPage", "checkReadStatus"))
                 .excludePathPatterns("/toLogin", "/image/code", "/doLogin", "/getMsgCode", "/doForget")
                 .excludePathPatterns("/static/**", "/js/**", "/css/**", "/img/**", "/plugins/**", "/vendor/**", "/error/**", "/image/**", "/upfiles/**");
-    }*/
+    }
 }
